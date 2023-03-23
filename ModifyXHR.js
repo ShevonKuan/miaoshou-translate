5c06133f9cf1299aa8bc80c3bbc2a18de6f657e2
ShevonKuan <331749486@qq.com>
2023-03-24 02:57:37 +0800

    new file:   ModifyXHR.js

---
+++ ModifyXHR.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name          Modify XHR Request URL
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       ShevonKwan
+// @match        https://erp.91miaoshou.com/common_collect_box/items
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=91miaoshou.com
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function () {
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {
+        if (url === "https://erp.91miaoshou.com/api/move/common_collect_box/translateCommonBoxDetail") {
+            var newUrl = "http://192.168.0.1:9876";
+            var headers = arguments[5];
+            headers = headers ? headers : {};
+            headers["Referer"] = "https://erp.91miaoshou.com";
+            arguments[5] = headers;
+            open.call(this, method, newUrl, async, user, pass);
+        } else {
+            open.call(this, method, url, async, user, pass);
+        }
+    };
+})();
+
+
+
