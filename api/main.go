82d9ee2166bea6660da10e8f44751663c73a3b8f
ShevonKuan <331749486@qq.com>
2023-03-24 02:50:14 +0800

    modified:   api/main.go

--- api/main.go
+++ api/main.go
@@ -90,6 +90,8 @@ func Api(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	// cors
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 	w.Write([]byte(resp))
 }
