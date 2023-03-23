abf651b5170b8935dbd1cb4206988eae04463727
ShevonKuan <331749486@qq.com>
2023-03-24 02:54:49 +0800

    modified:   api/main.go

--- api/main.go
+++ api/main.go
@@ -92,6 +92,6 @@ func Api(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "https://erp.91miaoshou.com")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Write([]byte(resp))
 }
